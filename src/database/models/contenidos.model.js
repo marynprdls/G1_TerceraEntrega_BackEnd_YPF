835f39a9f4e1bac89485edfeee89c24a24b48f60
marynprdls <38818355+marynprdls@users.noreply.github.com>
2024-08-06 14:47:51 -0300

    agregado mapa database

--- src/database/models/contenidos.model.js
+++ src/database/models/contenidos.model.js
@@ -1,77 +1,43 @@
 module.exports = (sequelize, Sequelize, DataTypes) => {  
 
-    const Contenido = sequelize.define( 
-
-        "contenidos",
-
-        {
-
-            id: {
-
-                type: DataTypes.INTEGER,
-
-                
-
-                primaryKey: true,
-
-                autoIncrement: true,
-
-                allowNull: false
-
-            }, 
-
-            poster: {
-
-                type: DataTypes.STRING(255),
-
-                allowNull: false
-
-            },
-
-            titulo: {
-
-                type: DataTypes.STRING(100),
-
-                allowNull: false
-
-            },
-
-            resumen: {
-
-                type: DataTypes.STRING,
-
-                allowNull: false
-
-            },
-            temporadas: {
-
-                type: DataTypes.INTEGER,
-
-                allowNull: true
-
-            },
-
-            trailer: {
-
-                type: DataTypes.STRING(255),
-
-                //allowNull: false
-
-            }
+    const Contenido = sequelize.define( "contenidos",
+    { 
+        id: {
+             type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        }, 
+        
+        poster: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        
+        titulo: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+        },
+        
+        resumen: {
+        type: DataTypes.STRING,
+        allowNull: false
+        },
+        temporadas: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+        },
+        trailer: {
+        type: DataTypes.STRING(255),
+        //allowNull: false
+        }
     },
-
-    {
-
+    { 
         timestamps: true,
-
         underscored: true,
-
     }
 
 );
-
-//module.exports = contenidos;
-
 return Contenido;
 
 }
